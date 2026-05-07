ece2e4a857eb504df202c94790fea58b6b2951ed
maccuaphuc <phucphuc@MacBook-Pro-cua-maccuaphuc.local>
2026-05-07 18:37:20 +0700

    fix dbconfig

--- BE/backend/src/main/java/util/DatabaseConfig.java
+++ BE/backend/src/main/java/util/DatabaseConfig.java
@@ -4,12 +4,12 @@ public class DatabaseConfig {
 
     // Đường dẫn kết nối đến MySQL
     public static final String URL = EnvConfig.get(
-            "DB_URL",
-            "jdbc:mysql://localhost:3306/laptop_store?serverTimezone=UTC&useUnicode=true&characterEncoding=utf-8"
+            "DB_URL",""
+            
     );
 
     // Tên đăng nhập MySQL
-    public static final String USERNAME = EnvConfig.get("DB_USERNAME", "root");
+    public static final String USERNAME = EnvConfig.get("DB_USERNAME", "");
 
     // Mật khẩu MySQL
     public static final String PASSWORD = EnvConfig.get("DB_PASSWORD", "");
